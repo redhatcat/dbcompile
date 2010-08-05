7339f8a3cf04240e69774449875bc1db76284836
Tyler Lesmann <redhatcat@gmail.com>
2010-08-05 16:41:05 -0600

    Function verification

--- lib/dbcompile/function.rb
+++ lib/dbcompile/function.rb
@@ -3,5 +3,17 @@ module DbCompile
     def build_path
       @path = File.join('functions', "#{name}.sql")
     end
+
+    def verify
+      sql = nil
+      case ActiveRecord::Base.connection.class.to_s
+        when 'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter'
+          sql = "select proname from pg_proc where proname = '#{name.downcase}';"
+      end
+      if sql
+        result = ActiveRecord::Base.connection.execute(sql)
+        return result.rows.length == 1
+      end
+    end
   end
 end
