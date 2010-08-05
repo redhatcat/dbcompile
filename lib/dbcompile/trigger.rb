a05d8cb3559d7ed7749b2f0f504f7e791f24052e
Tyler Lesmann <redhatcat@gmail.com>
2010-08-05 16:32:14 -0600

    Verification for trigger creation.

--- lib/dbcompile/trigger.rb
+++ lib/dbcompile/trigger.rb
@@ -3,5 +3,17 @@ module DbCompile
     def build_path
       @path = File.join('triggers', "#{name}.sql")
     end
+
+    def verify
+      sql = nil
+      case ActiveRecord::Base.connection.class.to_s
+        when 'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter'
+          sql = "select * from pg_trigger where tgname = '#{name}';"
+      end
+      if sql
+        result = ActiveRecord::Base.connection.execute(sql)
+        return result.rows.length == 1
+      end
+    end
   end
 end
